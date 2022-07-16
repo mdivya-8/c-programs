ac546dc5f51b3a9dd1e4ca450867dabc8f43fd45
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-07-16 21:32:29 +0530

    enumerations

---
+++ enum_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+enum State {Working = 1, Failed = 0, Freezed = 0}; //two enum names can have same values
+
+int main()
+{
+printf("%d, %d, %d", Working, Failed, Freezed);
+
+}
+
---
+++ enum_5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+enum state {working, failed};
+enum result {failed, passed}; //All enum constants must be unique in their scope. program fails in compilation.
+
+int main() 
+{
+    
+}
